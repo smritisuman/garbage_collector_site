c8d99a6482cea0da99edf3fa3efa44a4e1ceaf89
SMRITI <87012015+smritisuman@users.noreply.github.com>
2022-05-19 12:47:42 +0530

    Add files via upload

---
+++ Images/try.js
@@ -0,0 +1,35 @@
+$("#st").click(function() {
+    $("#stBlurb").fadeToggle();
+    //   $("#div2").fadeToggle("slow");
+    //    $("#div3").fadeToggle(3000);
+});
+
+$("#ver").click(function() {
+    $("#verBlurb").fadeToggle();
+    //   $("#div2").fadeToggle("slow");
+    //    $("#div3").fadeToggle(3000);
+});
+
+$("#bg").click(function() {
+    $("#bgBlurb").fadeToggle();
+    //   $("#div2").fadeToggle("slow");
+    //    $("#div3").fadeToggle(3000);
+});
+
+$("#blm").click(function() {
+    $("#blmBlurb").fadeToggle();
+    //   $("#div2").fadeToggle("slow");
+    //    $("#div3").fadeToggle(3000);
+});
+
+$("#nykop").click(function() {
+    $("#nykopBlurb").fadeToggle();
+    //   $("#div2").fadeToggle("slow");
+    //    $("#div3").fadeToggle(3000);
+});
+
+$("#slb").click(function() {
+    $("#slbBlurb").fadeToggle();
+    //   $("#div2").fadeToggle("slow");
+    //    $("#div3").fadeToggle(3000);
+});
